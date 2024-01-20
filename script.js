1da5a10b6c65b770a21d3db4226d3b8beab2a2c8
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2024-01-20 13:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+// script.js
+
+function manipulateData(inputArray) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+  });
+}
+
+function filterOddNumbers(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const filteredArray = data.filter((num) => num % 2 === 0);
+      resolve(filteredArray);
+    }, 1000);
+  });
+}
+
+function multiplyEvenNumbers(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const multipliedArray = data.map((num) => (num % 2 === 0 ? num * 2 : num));
+      resolve(multipliedArray);
+    }, 2000);
+  });
+}
+
+function updateOutputElement(result) {
+  const outputElement = document.getElementById('output');
+  outputElement.textContent = result.join(', ');
+}
+
+const inputData = [1, 2, 3, 4];
+
+manipulateData(inputData)
+  .then(filterOddNumbers)
+  .then(multiplyEvenNumbers)
+  .then(updateOutputElement);
